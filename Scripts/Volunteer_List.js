d9d84e9ef0281665562c48d0cc2afa6904098dfd
itai <121659052+Itai-L@users.noreply.github.com>
2023-04-11 12:58:09 +0300

    Add files via upload

--- Scripts/Volunteer_List.js
+++ Scripts/Volunteer_List.js
@@ -1,155 +1,255 @@
 
-
-
+let volunteersArray = [];
 const volunteersTable = document.getElementById('volunteer-list');
+ const db = firebase.firestore();
+  const volunteersRef = db.collection("Volunteers");
+  const filterBar = document.getElementById("authorization-filter");
 
+  async function readVolunteers() {
+  // Get the Firestore instance and the "Volunteers" collection
 
-// Get all documents from the "Volunteers" collection
-firebase.firestore().collection("Volunteers").get().then((querySnapshot) => {
-  querySnapshot.forEach((doc) => {
-    const volunteerData = doc.data();
-    
-    // Create a new row in the table for each volunteer
-    const row = document.createElement('tr');
-    const nameCell = document.createElement('td');
-    nameCell.textContent = volunteerData.firstName + " " + volunteerData.lastName;
-    const emailCell = document.createElement('td');
-    emailCell.textContent = volunteerData.email;
-    row.appendChild(nameCell);
-    row.appendChild(emailCell);
-    volunteersTable.appendChild(row);
+  try {
+    // Get all the documents in the "Volunteers" collection
+    const snapshot = await volunteersRef.get();
 
-    // Add a click event listener to each row to show more details
-    row.addEventListener('click', () => {
-      // Check if the details row already exists
-      const detailsRow = row.nextElementSibling;
-      if (detailsRow && detailsRow.classList.contains('volunteer-details-row')) {
-        // If the row already exists, remove it to hide the details
-        row.parentElement.removeChild(detailsRow);
+    // Loop through each document
+    snapshot.forEach((doc) => {
+      // If the document exists, add the data to the volunteersArray
+      if (doc.exists) {
+        volunteersArray.push(doc.data());
       } else {
-        // Create a new row to display the volunteer details
-        const detailsRow = document.createElement('tr');
-        detailsRow.classList.add('volunteer-details-row');
-        const detailsCell = document.createElement('td');
-        detailsCell.colSpan = 8;
-        const detailsList = document.createElement('ul');
-        const firstNameItem = document.createElement('li');
-        firstNameItem.textContent = "First Name: " + volunteerData.firstName;
-        const lastNameItem = document.createElement('li');
-        lastNameItem.textContent = "Last Name: " + volunteerData.lastName;
-        const IDItem = document.createElement('li');
-        IDItem.textContent = "ID: " + volunteerData.ID;
-        const emailItem = document.createElement('li');
-        emailItem.textContent = "Email: " + volunteerData.email;
-        const phoneItem = document.createElement('li');
-        phoneItem.textContent = "Phone: " + volunteerData.phone;
-        const addressItem = document.createElement('li');
-        addressItem.textContent = "Address: " + volunteerData.address;
-        const authItem = document.createElement('li');
-        authItem.textContent = "Authorizations: " + volunteerData.Authorizations;
-        const volunteerTypesItem = document.createElement('li');
-        volunteerTypesItem.textContent = "Volunteer Types: ";
-        const volunteerTypesList = document.createElement('ul');
-        volunteerData.volunteerTypes.forEach(type => {
-          const typeItem = document.createElement('li');
-          typeItem.textContent = type;
-          volunteerTypesList.appendChild(typeItem);
-        });
-        volunteerTypesItem.appendChild(volunteerTypesList);
-        
-        const vehiclesItem = document.createElement('li');
-        vehiclesItem.textContent = "Vehicles: ";
-        const vehiclesList = document.createElement('ul');
-        volunteerData.vehicles.forEach(vehicle => {
-          const vehicleItem = document.createElement('li');
-          vehicleItem.textContent = vehicle;
-          vehiclesList.appendChild(vehicleItem);
-        });
-        vehiclesItem.appendChild(vehiclesList);
-        
-        vehiclesItem.appendChild(vehiclesList);
-        detailsList.appendChild(firstNameItem);
-        detailsList.appendChild(lastNameItem);
-        detailsList.appendChild(IDItem);
-        detailsList.appendChild(emailItem);
-        detailsList.appendChild(phoneItem);
-        detailsList.appendChild(addressItem);
-        detailsList.appendChild(authItem);
-        detailsList.appendChild(volunteerTypesItem);
-        detailsList.appendChild(vehiclesItem);
-        detailsCell.appendChild(detailsList);
-        detailsRow.appendChild(detailsCell);
-        row.after(detailsRow);
+        console.log("No such document!");
+      }
+    });
+  } catch (error) {
+    console.log("Error getting documents:", error);
+  }
+  preDisplayVolunteers("all");
+  // Return the volunteersArray
+}
 
-        const manageAuthButton = document.createElement('button');
-manageAuthButton.textContent = 'Manage Authorizations';
-detailsList.appendChild(manageAuthButton);
+
+// Add an event listener to detect changes in the filter bar selection
+filterBar.addEventListener("change", function() {
+  const selectedValue = filterBar.value;
+  console.log(selectedValue);
+   preDisplayVolunteers(selectedValue);
+});
+
+
+function preDisplayVolunteers(filter)
+{
+  volunteersTable.innerHTML="";
+ if(filter== "Manager")
+ filter='0';
+ 
+  volunteersArray.forEach((volunteerData) => {
+    if(filter=="all"){
+    DisplayVolunteers(volunteerData);
+    }else{
+   let AuthString=volunteerData.Authorizations;
+    if(AuthString.toLowerCase().includes(filter.toLowerCase()))
+      DisplayVolunteers(volunteerData);
+    }
+  })
+}
+
+
+
+
+
+function DisplayVolunteers(volunteerData)
+{
+  const row = document.createElement('tr');
+  const nameCell = document.createElement('td');
+  nameCell.textContent = volunteerData.firstName + " " + volunteerData.lastName;
+  const emailCell = document.createElement('td');
+  emailCell.textContent = volunteerData.email;
+  row.appendChild(nameCell);
+  row.appendChild(emailCell);
+  volunteersTable.appendChild(row);
+
+  // Add a click event listener to each row to show more details
+  row.addEventListener('click', () => {
+    // Check if the details row already exists
+    const detailsRow = row.nextElementSibling;
+    if (detailsRow && detailsRow.classList.contains('volunteer-details-row')) {
+      // If the row already exists, remove it to hide the details
+      row.parentElement.removeChild(detailsRow);
+    } else {
+      // Create a new row to display the volunteer details
+      const detailsRow = document.createElement('tr');
+      detailsRow.classList.add('volunteer-details-row');
+      const detailsCell = document.createElement('td');
+      detailsCell.colSpan = 8;
+      const detailsList = document.createElement('ul');
+      const firstNameItem = document.createElement('li');
+      firstNameItem.textContent = "First Name: " + volunteerData.firstName;
+      const lastNameItem = document.createElement('li');
+      lastNameItem.textContent = "Last Name: " + volunteerData.lastName;
+      const IDItem = document.createElement('li');
+      IDItem.textContent = "ID: " + volunteerData.ID;
+      const emailItem = document.createElement('li');
+      emailItem.textContent = "Email: " + volunteerData.email;
+      const phoneItem = document.createElement('li');
+      phoneItem.textContent = "Phone: " + volunteerData.phone;
+      const addressItem = document.createElement('li');
+      addressItem.textContent = "Address: " + volunteerData.address;
+      const authItem = document.createElement('li');
+      authItem.textContent = "Authorizations: " + volunteerData.Authorizations;
+      const volunteerTypesItem = document.createElement('li');
+      volunteerTypesItem.textContent = "Volunteer Types: ";
+      const volunteerTypesList = document.createElement('ul');
+      volunteerData.volunteerTypes.forEach(type => {
+        const typeItem = document.createElement('li');
+        typeItem.textContent = type;
+        volunteerTypesList.appendChild(typeItem);
+      });
+      volunteerTypesItem.appendChild(volunteerTypesList);
+      
+      const vehiclesItem = document.createElement('li');
+      vehiclesItem.textContent = "Vehicles: ";
+      const vehiclesList = document.createElement('ul');
+      volunteerData.vehicles.forEach(vehicle => {
+        const vehicleItem = document.createElement('li');
+        vehicleItem.textContent = vehicle;
+        vehiclesList.appendChild(vehicleItem);
+      });
+      vehiclesItem.appendChild(vehiclesList);
+      
+      vehiclesItem.appendChild(vehiclesList);
+      detailsList.appendChild(firstNameItem);
+      detailsList.appendChild(lastNameItem);
+      detailsList.appendChild(IDItem);
+      detailsList.appendChild(emailItem);
+      detailsList.appendChild(phoneItem);
+      detailsList.appendChild(addressItem);
+      detailsList.appendChild(authItem);
+      detailsList.appendChild(volunteerTypesItem);
+      detailsList.appendChild(vehiclesItem);
+      detailsCell.appendChild(detailsList);
+      detailsRow.appendChild(detailsCell);
+      row.after(detailsRow);
+
+      //authorization button;
+    
+    const manageAuthButton = document.createElement('button');
+
+    manageAuthButton.textContent = 'Manage Authorizations';
+    detailsList.appendChild(manageAuthButton);
 
 // Add a click event listener to the "Manage Authorizations" button
 manageAuthButton.addEventListener('click', () => {
-  // Check if the authorizations row already exists
-  const authRow = manageAuthButton.parentElement.nextElementSibling;
-  if (authRow && authRow.classList.contains('volunteer-auth-row')) {
-    // If the row already exists, remove it to hide the authorizations
-    authRow.parentElement.removeChild(authRow);
-  } else {
-    // Create a new row to display the authorization checkboxes
-    const authRow = document.createElement('tr');
-    authRow.classList.add('volunteer-auth-row');
-    const authCell = document.createElement('td');
-    authCell.colSpan = 8;
-    const authForm = document.createElement('form');
-    const authCheckboxes = [
-      { name: 'Manager', value: 'manager' },
-      { name: 'Transit', value: 'transit' },
-      { name: 'Telephone', value: 'telephone' },
-      { name: 'Jeep', value: 'jeep' },
-      { name: 'General', value: 'general' }
-    ];
-    authCheckboxes.forEach(cb => {
-      const cbLabel = document.createElement('label');
-      const cbInput = document.createElement('input');
-      cbInput.type = 'checkbox';
-      cbInput.name = cb.value;
-      cbInput.value = cb.value;
-      if (volunteerData.Authorizations.includes(cb.value)) {
-        cbInput.checked = true;
-      }
-      cbLabel.appendChild(cbInput);
-      cbLabel.append(cb.name);
-      authForm.appendChild(cbLabel);
-    });
-    const submitButton = document.createElement('button');
-    submitButton.textContent = 'Submit';
-    authForm.appendChild(submitButton);
-    
-    // Add a click event listener to the "Submit" button
-    submitButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      const selectedAuths = Array.from(authForm.querySelectorAll('input[type=checkbox]:checked')).map(cb => cb.value).join(', ');
-      doc.ref.update({
-        Authorizations: selectedAuths
-      }).then(() => {
-        authItem.textContent = "Authorizations: " + selectedAuths;
-        authRow.parentElement.removeChild(authRow);
-      }).catch((error) => {
-        console.error("Error updating document: ", error);
-      });
-    });
-    
-    authCell.appendChild(authForm);
-    authRow.appendChild(authCell);
-    manageAuthButton.parentElement.parentElement.after(authRow);
+// Check if the authorizations row already exists
+const authRow = manageAuthButton.parentElement.nextElementSibling;
+if (authRow && authRow.classList.contains('volunteer-auth-row')) {
+// If the row already exists, remove it to hide the authorizations
+authRow.parentElement.removeChild(authRow);
+} else {
+// Create a new row to display the authorization checkboxes
+const authRow = document.createElement('tr');
+authRow.classList.add('volunteer-auth-row');
+const authCell = document.createElement('td');
+authCell.colSpan = 8;
+const authForm = document.createElement('form');
+const authCheckboxes = [
+  { name: 'Manager', value: '0' },
+  { name: 'Transit', value: 'Transit' },
+  { name: 'Telephone', value: 'Telephone' },
+  { name: 'Jeep', value: 'Jeep' },
+  { name: 'General', value: 'General' }
+];
+authCheckboxes.forEach(cb => {
+  const cbLabel = document.createElement('label');
+  const cbInput = document.createElement('input');
+  cbInput.type = 'checkbox';
+  cbInput.name = cb.value;
+  cbInput.value = cb.value;
+  if (volunteerData.Authorizations.includes(cb.value)) {
+    cbInput.checked = true;
   }
+  cbLabel.appendChild(cbInput);
+  cbLabel.append(cb.name);
+  authForm.appendChild(cbLabel);
 });
+const submitButton = document.createElement('button');
+submitButton.textContent = 'Submit';
+authForm.appendChild(submitButton);
 
-
-
-
-        
-      }
-    });
+// Add a click event listener to the "Submit" button
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  const selectedAuths = Array.from(authForm.querySelectorAll('input[type=checkbox]:checked')).map(cb => cb.value).join(', ');
+  const docRef = db.collection("Volunteers").doc(volunteerData.email);
+  docRef.update({
+    Authorizations: selectedAuths
+  }).then(() => {
+    authItem.textContent = "Authorizations: " + selectedAuths;
+    authRow.parentElement.removeChild(authRow);
+  }).catch((error) => {
+    console.error("Error updating document: ", error);
   });
 });
+
+authCell.appendChild(authForm);
+authRow.appendChild(authCell);
+manageAuthButton.parentElement.parentElement.after(authRow);
+}
+});
+
+
+
+}  
+
+})
+
+
+}
+
+
+window.addEventListener('load', () => {
+  document.getElementById('exportButton').addEventListener('click', () => {
+    const headers = ['First Name', 'Last Name', 'ID', 'Email', 'Phone', 'Address', 'Emergency Contact Name', 'Emergency Contact Phone', 'Birth Date'];
+    const rows = volunteersArray.map(item => [
+      item.firstName,
+      item.lastName,
+      item.ID,
+      item.email,
+      item.phone,
+      item.address,
+      item.EmergencyContactName,
+      item.EmergencyContactPhone,
+      item.BirthDate
+    ]);
+
+    // Combine headers and rows into plain text format
+    const data = [headers, ...rows].map(row => row.join('\t')).join('\n');
+
+    // Create a blob and download the file
+    const blob = new Blob([data], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'myData.txt';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
