47d4e688f7ae78145bef1146c8602121a665a39b
elisham1 <elishevak423@gmail.com>
2023-04-22 14:27:58 +0300

    תרגום העמודים

---
+++ Scripts/MainPage.js
@@ -0,0 +1,9 @@
+// document.addEventListener('click', function(event) {
+//     var dropdowns = document.getElementsByClassName('dropdown-content');
+//     for (var i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.style.display === 'block' && !openDropdown.contains(event.target)) {
+//         openDropdown.style.display = 'none';
+//       }
+//     }
+//   });
