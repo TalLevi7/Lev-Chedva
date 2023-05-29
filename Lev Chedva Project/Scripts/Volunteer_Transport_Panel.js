668c423dad46715b4d20c262a35a30b37e6a33a6
bar18340 <bar18340@users.noreply.github.com>
2023-05-29 14:20:27 +0300

    29.05.23
    מרכוז כפתורים בטבלאות שינוע, הגדלת שורות הטבלאות, הגדלת כפתורים

--- Lev Chedva Project/Scripts/Volunteer_Transport_Panel.js
+++ Lev Chedva Project/Scripts/Volunteer_Transport_Panel.js
@@ -116,9 +116,14 @@ function DisplayData(eventData) {
       detailsRow.appendChild(detailsCell);
       row.after(detailsRow);
 
+      const buttonContainer = document.createElement('div');
+      buttonContainer.style.display = 'flex';
+      buttonContainer.style.justifyContent = 'center';
+      buttonContainer.style.alignItems = 'center';
       const TakeEvent = document.createElement('button');
       TakeEvent.textContent = 'קח';
-      detailsList.appendChild(TakeEvent);
+      buttonContainer.appendChild(TakeEvent);
+      detailsList.appendChild(buttonContainer);
 
       TakeEvent.addEventListener('click', () => {
         var user = firebase.auth().currentUser;
