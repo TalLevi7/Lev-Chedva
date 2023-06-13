6b41f243c583a2b62276912cb8ec5266ff824e07
Your Name <you@example.com>
2023-06-13 19:48:31 +0300

    13/06

--- functions/index.js
+++ functions/index.js
@@ -13,24 +13,37 @@ let transporter = nodemailer.createTransport({
   }
 });
 
+
+  
+
 exports.sendEmailOnFormSubmit = functions.firestore
-  .document('Open Events/{documentId}')
-  .onCreate((snap, context) => {
-    const data = snap.data();
-    const link = "https://levchedva.org/Pages/login_HE.html";
-    const messageText = `שינוע חדש!מי זוכה בחסד? מכתובת ${data.Source_Address} לכתובת: ${data.Destination_Address}. היכנסו ללינק: ${link}`;
+.document('Open Events/{documentId}')
+.onCreate((change, context) => {
+  const data = change.after.exists ? change.after.data() : null;
+  const previousData = change.before.exists ? change.before.data() : null;
+  const link = "https://levchedva.org/Pages/login_HE.html";
+  let messageText;
+  
+  // Check if the event status changed from any value to "פתוח" (Open)
+  if (data && data.status === "פתוח" && previousData && previousData.status !== "פתוח") {
+    if (data.urgency === "Urgent") {
+      messageText = `שינוע דחוף חדש! מי זוכה בחסד? מכתובת ${data.Source_Address} לכתובת: ${data.Destination_Address}. היכנסו ללינק: ${link}`;
+    } else {
+      messageText = `שינוע חדש! מי זוכה בחסד? מכתובת ${data.Source_Address} לכתובת: ${data.Destination_Address}. היכנסו ללינק: ${link}`;
+    }
+
     const encodedMessage = encodeURIComponent(messageText);
     const phoneNumber = '+972545420068'; // Replace with the actual phone number
     const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
-    
+
     let mailOptions = {
       from: 'levchedvaalerts@gmail.com',  // sender
       to: 'levchedvaalerts@gmail.com',  // receiver
       subject: 'נפתח שינוע חדש',
       text: `${messageText}\n\n שלח לווצאפ ${whatsappLink}`,
-      
     };
 
+    // Send the email
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log(error);
@@ -38,77 +51,135 @@ exports.sendEmailOnFormSubmit = functions.firestore
         console.log('Email sent: ' + info.response);
       }
     });
-  });
+  }
+
+  return null;
+});
 
 
-  exports.sendEmailOnDelivered = functions.firestore
-  .document('Open Events/{documentId}')
-  .onUpdate((change, context) => {
-    // Get the after state of the document
-    const dataAfter = change.after.data();
 
-    // Check if status has changed to "נמסר"
-    if (dataAfter.status === "נמסר") {
-      // Prepare the mail message
-      const messageText = `השינוע מספר ${dataAfter.eventCounter} נמסר!`;
-      
-      let mailOptions = {
-        from: 'levchedvaalerts@gmail.com',  // sender
-        to: 'levchedvaalerts@gmail.com',  // receiver
-        subject: 'שינוע נמסר',
-        text: messageText
-      };
-      
-      // Send the mail
-      return transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-          return error;
-        } else {
-          console.log('Email sent: ' + info.response);
-          return info.response;
-        }
-      });
+
+
+
+
+
+
+exports.sendEmailOnFormSubmit = functions.firestore
+.document('Open Events/{documentId}')
+.onCreate((snap, context) => {
+  const data = snap.data();
+  const link = "https://levchedva.org/Pages/login_HE.html";
+  const messageText = `שינוע חדש!מי זוכה בחסד? מכתובת ${data.Source_Address} לכתובת: ${data.Destination_Address}. היכנסו ללינק: ${link}`;
+  const encodedMessage = encodeURIComponent(messageText);
+  const phoneNumber = '+972545420068'; // Replace with the actual phone number
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+  
+  let mailOptions = {
+    from: 'levchedvaalerts@gmail.com',  // sender
+    to: 'levchedvaalerts@gmail.com',  // receiver
+    subject: 'נפתח שינוע חדש',
+    text: `${messageText}\n\n שלח לווצאפ ${whatsappLink}`,
+    
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
     } else {
-      console.log('Status not changed to "נמסר". No email sent.');
-      return null;
+      console.log('Email sent: ' + info.response);
     }
   });
+});
 
 
-  exports.sendEmailOnCancelled = functions.firestore
-  .document('Open Events/{documentId}')
-  .onUpdate((change, context) => {
+exports.sendEmailsOnStatusChange = functions.firestore
+.document('Open Events/{documentId}')
+.onUpdate((change, context) => {
     // Get the after state of the document
     const dataAfter = change.after.data();
 
     // Check if status has changed to "פתוח"
     if (dataAfter.status === "פתוח") {
-      // Prepare the mail message
-      const messageText = `השינוע מספר ${dataAfter.eventCounter} בוטל!`;
-      
-      let mailOptions = {
-        from: 'levchedvaalerts@gmail.com',  // sender
-        to: 'levchedvaalerts@gmail.com',  // receiver
-        subject: 'שינוע בוטל',
-        text: messageText
-      };
-      
-      // Send the mail
-      return transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-          return error;
+        // Prepare the first mail message
+        const messageText1 = `השינוע מספר ${dataAfter.eventCounter} בוטל!`;
+
+        let mailOptions1 = {
+            from: 'levchedvaalerts@gmail.com',  // sender
+            to: 'levchedvaalerts@gmail.com',  // receiver
+            subject: 'שינוע בוטל',
+            text: messageText1
+        };
+
+        // Send the first mail
+        transporter.sendMail(mailOptions1, (error, info) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
+        // Prepare the second mail message
+        const link = "https://levchedva.org/Pages/login_HE.html";
+        let messageText2;
+
+        if (dataAfter.urgency === "Urgent") {
+            messageText2 = `שינוע דחוף חדש! מי זוכה בחסד? מכתובת ${dataAfter.Source_Address} לכתובת: ${dataAfter.Destination_Address}. היכנסו ללינק: ${link}`;
         } else {
-          console.log('Email sent: ' + info.response);
-          return info.response;
+            messageText2 = `שינוע חדש! מי זוכה בחסד? מכתובת ${dataAfter.Source_Address} לכתובת: ${dataAfter.Destination_Address}. היכנסו ללינק: ${link}`;
         }
-      });
+
+        const encodedMessage = encodeURIComponent(messageText2);
+        const phoneNumber = '+972545420068'; // Replace with the actual phone number
+        const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+        let mailOptions2 = {
+            from: 'levchedvaalerts@gmail.com',  // sender
+            to: 'levchedvaalerts@gmail.com',  // receiver
+            subject: 'נפתח שינוע חדש',
+            text: `${messageText2}\n\n שלח לווצאפ ${whatsappLink}`,
+        };
+
+        // Send the second email
+        return transporter.sendMail(mailOptions2, (error, info) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+
     } else {
-      console.log('Status not changed to "פתוח". No email sent.');
-      return null;
+        console.log('Status not changed to "פתוח". No emails sent.');
+        return null;
     }
+});
+
+  
+
+
+
+
+
+
+  exports.resetMonthlyEvents = functions.pubsub.schedule('0 0 14 * *')
+  .timeZone('Asia/Jerusalem') // Use your preferred timezone
+  .onRun(async (context) => {
+      const volunteersRef = admin.firestore().collection('Volunteers');
+      const snapshot = await volunteersRef.get();
+
+      // Start a batch
+      const batch = admin.firestore().batch();
+
+      snapshot.forEach((doc) => {
+          // For each document, add a operation to the batch that sets monthlyEvents to 0
+          batch.update(doc.ref, { monthlyEvents: 0 });
+      });
+
+      // Commit the batch
+      await batch.commit();
   });
+
 
 
 
